29ed91f15d965fbfb0ce0829a382c9d82de6c669
dopplershift <rmay31@gmail.com>
2011-02-27 02:40:28 -0600

    Refactor ScalarProduct solution to use the new base class.

--- ScalarProduct/ScalarProduct.pro
+++ ScalarProduct/ScalarProduct.pro
@@ -1,7 +1,8 @@
 TARGET = scalarproduct
 SOURCES = scalarproduct.cpp
+INCLUDEPATH += ..
 
 OTHER_FILES += \
     test_data.txt \
     A-small-practice.in \
-    A-large-practice.in
+    A-large-practice.in.
--- ScalarProduct/scalarproduct.cpp
+++ ScalarProduct/scalarproduct.cpp
@@ -1,57 +1,29 @@
+#include "CodeSolution.h"
+
 #include <algorithm>
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
 #include <iterator>
-#include <limits>
 #include <numeric>
-#include <string>
 #include <sstream>
 #include <vector>
 
 using namespace std;
 
-typedef long long Number;
-
-int main(int argc, char* argv[])
+class ScalarProductSolution : public CodeSolution
 {
-    if(argc < 2)
-    {
-        cerr << "Need to specify input file." << endl;
-        exit(EXIT_FAILURE);
-    }
+    typedef long long Number;
 
-    // Stream for saving output.
-    ostringstream out;
-
-    ifstream inFile(argv[1]);
-    if(!inFile)
-    {
-        cerr << "Error opening file: " << argv[1] << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    unsigned int numCases;
-    inFile >> numCases;
-
-    // Flush trailing newline
-    inFile.ignore(numeric_limits<streamsize>::max(),'\n');
-
-    for(unsigned int t = 0; t < numCases; ++t)
+    void solveCase()
     {
         // Flush line with vector size, since we don't use it
-        inFile.ignore(numeric_limits<streamsize>::max(),'\n');
+        getLine();
 
         // Read a line and parse into integers
         istream_iterator<Number> eos;
-        string line;
-        getline(inFile, line);
-        istringstream buf(line);
+        istringstream buf(getLine());
         vector<Number> vec1(istream_iterator<Number>(buf), eos);
 
         // Read second line into integers
-        getline(inFile, line);
-        buf.str(line);
+        buf.str(getLine());
         buf.clear();
         vector<Number> vec2(istream_iterator<Number>(buf), eos);
 
@@ -59,22 +31,13 @@ int main(int argc, char* argv[])
         sort(vec1.begin(), vec1.end());
         sort(vec2.begin(), vec2.end(), greater<Number>());
 
-        // Taking the inner product of the sorted arrays will give the minimumb
-        out << "Case #" << t + 1 << ": "
-                << inner_product(vec1.begin(), vec1.end(), vec2.begin(), 0L)
-                << endl;
+        // Taking the inner product of the sorted arrays will give the minimum
+        out << inner_product(vec1.begin(), vec1.end(), vec2.begin(), 0L);
     }
+};
 
-    // If we've been given an extra parameter, use this as the filename
-    // to write output to. Otherwise, just dump to standard out.
-    if(argc > 2)
-    {
-        ofstream outFile(argv[2]);
-        outFile << out;
-        outFile.close();
-    }
-    else
-        cout << out.str();
-
-    return 0;
+int main(int argc, char* argv[])
+{
+    ScalarProductSolution sps;
+    return sps.process(argc, argv);
 }
